ed037efcefbdae18f0416c647511c59fa7ae4605
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 10:43:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlog::Application.config.secret_token = '78c2dd261db427d8c0e626ea0cdc61591284b89c46aad6ca791ce497c8f90a2152754ac91aff1684851457b3c46bc1e1851aea7a1940bbc173f3c08737c473bf'
